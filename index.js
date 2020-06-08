f0de6aba7a241cf58d4e98ef5532da82507e3f41
Viktor Dzyubin <laacoste1@gmail.com>
2020-06-09 01:28:31 +0700

    add style to svg file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.onload = () => {
-	//code
+	const gamepadObject = document.querySelector('#gamepad');
+
+	console.log(gamepadObject)
+
+	const rightStick = gamepadObject.querySelectorAll('.right-stick')
+
+	console.log(rightStick)
 }
